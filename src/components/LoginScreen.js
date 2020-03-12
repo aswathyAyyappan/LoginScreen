1c0cc9c3895de399719e080a75c62cfbbefc798c
Aswathy Ayyappan <AAyyappan@spireon.com>
2020-03-12 12:18:25 +0530

    changes

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,36 +1,46 @@
 import React from 'react';
-import { Text, StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
-import {Avatar} from 'react-native-elements';
+import { Text, StyleSheet, View, ScrollView, TouchableOpacity, KeyboardAvoidingView, TextInput } from 'react-native';
+import { Avatar } from 'react-native-elements';
 import FloatLabelTextInput from 'react-native-floating-label-text-input';
 
 class LoginScreen extends React.Component {
     static navigationOptions = {
         headerShown: false
     }
+
+    state = {
+    };
+
     render() {
         return (
             <View style={styles.parentViewStyle}>
-                <ScrollView style={{ flex:1}}>
-                    <View style={styles.avatarViewStyle}>
-                        <Avatar rounded size="large" activeOpacity={0.7} title="A" />
-                    </View>
-                    <View style={styles.textViewStyle}>
+                <View style={styles.avatarViewStyle}>
+                    <Avatar rounded size="large" activeOpacity={0.7} title="A" overlayContainerStyle={{ backgroundColor: 'white' }} />
+                </View>
+                <View style={styles.textViewStyle}>
+                    <KeyboardAvoidingView behavior="padding" enabled>
                         <Text style={styles.loginStyle}>Login </Text>
-                        <View style={styles.textInputStyle}>
-                            <FloatLabelTextInput placeholder={"Email"} />
-                            <FloatLabelTextInput placeholder="Password" secureTextEntry={true} />
+                        <View style={styles.textInputViewStyle}>
+                            <TextInput style={styles.textInputStyle} placeholder={"Email"}
+                            />
+                            <TextInput style={styles.textInputStyle} placeholder="Password" secureTextEntry={true}
+                            />
+                            {/* <FloatLabelTextInput placeholder={"Email"} />
+                            <FloatLabelTextInput placeholder="Password" secureTextEntry={true} /> */}
                         </View>
-                        <TouchableOpacity 
-                        style={styles.submit}
-                        onPress={this._onPressButton}>
+                        <TouchableOpacity
+                            style={styles.submit}
+                            onPress={this._onPressButton}>
                             <Text style={styles.submitText}>GO!</Text>
                         </TouchableOpacity>
-                    </View>
-                </ScrollView>
+                    </KeyboardAvoidingView>
+                </View>
             </View>
         );
     }
+
 }
+
 
 const styles = StyleSheet.create({
     parentViewStyle: {
@@ -38,24 +48,22 @@ const styles = StyleSheet.create({
         // marginTop: 30,
         backgroundColor: `rgb(84, 147, 247)`
     },
-    avatarViewStyle : {
-        flex: 3,
+    avatarViewStyle: {
+        flex: 2,
         justifyContent: "center",
-        alignItems: 'center',
-        height: "30%"
+        alignItems: 'center'
     },
     textViewStyle: {
         flex: 2,
         backgroundColor: 'white',
         marginTop: 10,
-        marginBottom: 10,
-        marginLeft:10,
-        marginRight:10,
-        borderRadius: 10,
-        height: "70%"
+        marginBottom: 40,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10
     },
-    loginStyle:{
-        fontSize : 20,
+    loginStyle: {
+        fontSize: 20,
         fontWeight: 'bold',
         fontStyle: 'normal',
         textAlign: 'center',
@@ -63,25 +71,30 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         paddingBottom: 20
     },
-    textInputStyle:{
-        marginLeft:20,
-        marginRight:20
+    textInputViewStyle: {
+        marginLeft: 20,
+        marginRight: 20
     },
-    submit:{
-        marginRight:40,
-        marginLeft:40,
-        marginTop:40,
-        paddingTop:20,
-        paddingBottom:20,
-        backgroundColor:'#68a0cf',
-        borderRadius:20,
+    textInputStyle: {
+        height: 40, 
+        borderColor: 'gray', 
+        borderBottomWidth:1
+    },
+    submit: {
+        marginRight: 40,
+        marginLeft: 40,
+        marginTop: 40,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#68a0cf',
+        borderRadius: 20,
         borderWidth: 1,
         borderColor: '#fff'
-      },
-      submitText:{
-          color:'#fff',
-          textAlign:'center',
-      }
+    },
+    submitText: {
+        color: '#fff',
+        textAlign: 'center',
+    }
 });
 
 export default LoginScreen;
